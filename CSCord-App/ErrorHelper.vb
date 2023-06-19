e942d48f67271ae5d993ce9482ba38a4ba275302
Ayan <Ayan>
2023-06-19 14:52:15 +0500

    123

---
+++ CSCord-App/ErrorHelper.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Windows.Forms
+
+Public Class ErrorHelper
+    Sub ShowMb()
+        Try
+            MessageBox.Show("There was an error!", "Error Helper")
+        Catch ex As Exception
+            Debug.Write("Unable to show MessageBox!")
+
+        End Try
+
+
+    End Sub
+
+End Class
